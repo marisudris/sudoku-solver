eaae11832abe9ffb8eee04a79a9ee3c5a2d5af8c
Maris Udris <maris.udris.lv@gmail.com>
2021-08-14 18:34:55 +0300

    Add and setup webpack + webpack dev server

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 8080,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
+};
