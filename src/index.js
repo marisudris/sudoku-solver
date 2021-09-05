d7c5014e62d01f32842426750c3dedfb117ceb54
Maris Udris <maris.udris.lv@gmail.com>
2021-09-05 21:08:26 +0300

    Update - move js source into a separate dir

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 import './css/style.css';
-import { handleSubmit, handleModalClick, handleEscape } from './handlers.js';
-import { solverForm, modal } from './elements.js';
+import { handleSubmit, handleModalClick, handleEscape } from './js/handlers.js';
+import { solverForm, modal } from './js/elements.js';
 
 solverForm.addEventListener('submit', handleSubmit);
 modal.addEventListener('click', handleModalClick);
