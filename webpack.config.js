f6f6014935a2d26970a984aad78e72d3077698a3
Maris Udris <maris.udris.lv@gmail.com>
2021-09-05 21:14:34 +0300

    Update - renamed html template file

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html',
+            template: './src/template.html',
         }),
         new MiniCssExtractPlugin({
             filename: 'css/[name].[contenthash].css',
